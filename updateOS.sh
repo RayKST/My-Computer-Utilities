50cd9d1c8dec6e800e7c207f22c68e06b754bab3
Kauan <jailma.kmp@gmail.com>
2022-02-25 16:36:15 -0300

    updateOs update (:

--- updateOS.sh
+++ updateOS.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 sudo apt-get update
+echo "Update Done"
 sudo apt-get upgrade
+echo "Upgrade Done"
 sudo apt-get autoremove
+echo "Autoremove Done, reboot system..."
 sudo reboot
 
 # command to update os and reboot system
